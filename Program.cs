2cc1ce49e21cb3616c3ed95f73d1eef5a2b3e724
Cristofer Zavala <zcristofer993mail.com>
2026-04-18 16:03:42 -0600

    Primer push con estructura base de proyecto y configuracion de gitignore

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+// ============================================
+// Estado: Mensaje de bienvenida
+// ============================================
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+        Console.WriteLine("==============================================");
+        Console.WriteLine("     SISTEMA DE GESTIÓN DE INVENTARIO        ");
+        Console.WriteLine("==============================================");
+        Console.WriteLine();
+        Console.WriteLine($"Version:{version}");
+        Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+        Console.WriteLine($".NET Version: {Environment.Version}");
+        Console.WriteLine();
+        Console.WriteLine("Estructura del Proyecto");
+        Console.WriteLine("Configuracion .csproj");
+        Console.WriteLine("Carpeta srs creada");
+        Console.WriteLine("Metadatos Configurados");
+        Console.WriteLine();
+        Console.WriteLine("Proximo paso: Agregar argumentos CLI y configuracion de repositorio en github");
+
+
